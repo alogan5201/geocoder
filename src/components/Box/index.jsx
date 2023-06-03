6b3b9a2f26353ca81f27706236457819e1efbf73
alogan5201 <andrewthomaslogan@gmail.com>
2023-06-03 15:18:45 -0400

    up

--- src/components/Box/index.jsx
+++ src/components/Box/index.jsx
@@ -12,56 +12,47 @@ Coded by www.creative-tim.com
 
 * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 */
+import { forwardRef } from "react"; // prop-types is a library for typechecking of props
 
-import { forwardRef } from "react";
+import PropTypes from "prop-types"; // Custom styles for Box
 
-// prop-types is a library for typechecking of props
-import PropTypes from "prop-types";
+import MKBoxRoot from "components/Box/MKBoxRoot";
+const Box = forwardRef(({
+  variant,
+  bgColor,
+  color,
+  opacity,
+  borderRadius,
+  shadow,
+  coloredShadow,
+  ...rest
+}, ref) => <MKBoxRoot {...rest} ref={ref} ownerState={{
+  variant,
+  bgColor,
+  color,
+  opacity,
+  borderRadius,
+  shadow,
+  coloredShadow
+}} />); // Setting default values for the props of Box
 
-// Custom styles for Test
-import MKBoxRoot from "components/Test/MKBoxRoot";
-
-
-const Test = forwardRef(
-  ({ variant, bgColor, color, opacity, borderRadius, shadow, coloredShadow, ...rest }, ref) => (
-    <MKBoxRoot
-      {...rest}
-      ref={ref}
-      ownerState={{ variant, bgColor, color, opacity, borderRadius, shadow, coloredShadow }}
-    />
-  )
-);
-
-// Setting default values for the props of Test
-Test.defaultProps = {
+Box.defaultProps = {
   variant: "contained",
   bgColor: "transparent",
   color: "dark",
   opacity: 1,
   borderRadius: "none",
   shadow: "none",
-  coloredShadow: "none",
-};
+  coloredShadow: "none"
+}; // Typechecking props for the Box
 
-// Typechecking props for the Test
-Test.propTypes = {
+Box.propTypes = {
   variant: PropTypes.oneOf(["contained", "gradient"]),
   bgColor: PropTypes.string,
   color: PropTypes.string,
   opacity: PropTypes.number,
   borderRadius: PropTypes.string,
   shadow: PropTypes.string,
-  coloredShadow: PropTypes.oneOf([
-    "primary",
-    "secondary",
-    "info",
-    "success",
-    "warning",
-    "error",
-    "light",
-    "dark",
-    "none",
-  ]),
+  coloredShadow: PropTypes.oneOf(["primary", "secondary", "info", "success", "warning", "error", "light", "dark", "none"])
 };
-
-export default Test;
+export default Box;
