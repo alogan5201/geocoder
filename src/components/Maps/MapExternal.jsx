050c1885cb0d03afee417c9c73d98a140f23cb8b
alogan5201 <drewthomaslogan5201@gmail.com>
2023-06-10 20:50:07 -0400

    up

--- src/components/Maps/MapExternal.jsx
+++ src/components/Maps/MapExternal.jsx
@@ -5,7 +5,7 @@ import "react-tabs/style/react-tabs.css";
 import useStore from "store/mapStore";
 import LocationButton from "./LocationButton";
 import styles from "./custom-marker-and-popup.module.css";
-import { useMeasure, useCss } from "react-use";
+import { useMeasure, useCss, useEffectOnce, usePrevious } from "react-use";
 
 const center = [37.09024, -95.712891];
 const points = [
@@ -20,6 +20,7 @@ const MyMarkers = () => {
   const markerData = useStore((state) => state.markerData);
   const [markerPoints, setMarkerPoints] = useState(null);
   const [popupOpen, setPopupOpen] = useState(false);
+  const [currentCoords, setCurrentCoords] = useState(null);
   useEffect(() => {
     if (markerData) {
       setMarkerPoints(markerData);
@@ -42,6 +43,7 @@ const PointMarker = ({ center, content, openPopup }) => {
   const [position, setPosition] = useState(null);
   const [popupOpened, setPopupOpened] = useState(false);
   const [popupVisible, setPopupVisible] = useState("hidden");
+  const [rendered,setRendered] = useState(false)
   const popupStyles = useCss({
     visibility: popupVisible,
     animation: "fadeIn ease 1s",
@@ -50,48 +52,68 @@ const PointMarker = ({ center, content, openPopup }) => {
     OAnimation: "fadeIn ease 1s",
     msAnimation: "fadeIn ease 1s",
   });
-  const map = useMapEvents({
-    popupopen(e) {
+    const map = useMapEvents({
+      popupopen(e) {},
+      popupclose(e) {},
+    });
 
-      // setPosition(e.target._popup._latlng);
-      /*     var px = map.project(e.target._popup._latlng);
-      // find the height of the popup container, divide by 2, subtract from the Y axis of marker location
-      px.y -= e.target._popup._container.clientHeight / 2;
-      console.log("POPUP OPEN!", px);
-      map.panTo(map.unproject(px), { animate: true }); */
-    },
-    popupclose(e) {
-      setTimeout(() => {
-        //map-popup-show map-popup-hide
-        setPopupVisible("hidden");
-      }, 1000);
-    },
-  });
+const togglePopup = (open,markerRef) => {
+if(open){
+  markerRef.current.closePopup()
+markerRef.current.openPopup()
+}
+else {
+  markerRef.current.openPopup()
+
+}
+}
   const markerRef = useRef(null);
   useEffect(() => {
     if (openPopup) {
       let open = markerRef.current.isPopupOpen();
-  
+
       //  map.fitBounds([[center.lat, center.lng]], {padding: [50, 50], maxZoom: 13})
 
-    map.fitBounds([[center.lat, center.lng]], { padding: [50, 50], maxZoom: 13 });
-        markerRef.current.openPopup();
-           var px = map.project(markerRef.current._popup._latlng);
-           // find the height of the popup container, divide by 2, subtract from the Y axis of marker locations
-           px.y -= markerRef.current._popup._container.clientHeight / 2;
-           console.log("POPUP OPEN!", px);
-           map.panTo(map.unproject(px), { animate: true, duration: 0.2, easeLinearity: 0.5 });
+    map.fitBounds([[center.lat, center.lng]], {animate:true, padding: [50, 50], maxZoom: 13 });
+      map.getCenter();
+        togglePopup(open,markerRef)
+      
+           
+           if(rendered){
+                 var px = map.project(markerRef.current._popup._latlng);
+                 // find the height of the popup container, divide by 2, subtract from the Y axis of marker locations
+                 px.y -= markerRef.current._popup._container.clientHeight / 2;
+                 console.log("rendered", px.y, px);
+                 
+                   map.panTo(map.unproject(px), {
+                     animate: true,
+                     duration: 0.8,
+                     easeLinearity: 0.5,
+                   });
+            }
+            else {
+              setTimeout(() => {
+                setRendered(true)
+                     var px = map.project(markerRef.current._popup._latlng);
+                     // find the height of the popup container, divide by 2, subtract from the Y axis of marker locations
+                     px.y -= markerRef.current._popup._container.clientHeight / 2;
+                       console.log("first render", px.y);
+                map.getCenter();
+                map.panTo(map.unproject(px), { animate: true, duration: 0.8, easeLinearity: 0.5 });
+
+            }, 10);
+           }
     }
-  }, [map, center, openPopup]);
+  }, [map, center, openPopup,rendered]);
 
   function onPopupOpen() {
-    console.log("onPopupOpen");
+    
     setPopupOpened(true);
   }
 
   return (
     <Marker ref={markerRef} position={center}>
-      <Popup minWidth={300} position={position} className={styles.newPopup} keepInView={false} autoPan={true}>
+      <Popup minWidth={300} position={position} className={styles.newPopup} keepInView={true} autoPan={true}>
         <PopupMarker content={content} />
       </Popup>
     </Marker>
@@ -113,7 +135,6 @@ const MapExternal = () => {
         zoom={3}
         scrollWheelZoom={true}
         zoomControl={false}
-        placeholder={<PlaceHolder />}
       >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
