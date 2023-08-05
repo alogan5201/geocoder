e6c6f7db6bd2e70da766f405d342397ef61c18ff
alogan5201 <drewthomaslogan5201@gmail.com>
2023-08-05 04:19:34 -0400

    up

--- src/components/FormWrapper/index.jsx
+++ src/components/FormWrapper/index.jsx
@@ -6,6 +6,8 @@ import { useState, lazy, useEffect } from 'react';
 import useStore from 'store/mapStore';
 //import Button from 'components/Button';
 import LocationButtonIcon from 'components/LocationButtonIcon';
+import { SyncLoader } from 'react-spinners';
+
 const MapExternal = lazy(() => import('components/Maps/MapExternal'));
 
 function FormWrapper({ form }) {
@@ -38,7 +40,33 @@ function FormWrapper({ form }) {
   };
   return (
     <>
-      <Box component="section" py={{ xs: 2, sm: 6 }} sx={{ maxWidth: '100%' }}>
+      {!mapLoaded && (
+        <Grid container item px={0}>
+          <Box width="100%" bgColor="white" mt={4} mb={6} sx={{ overflow: 'hidden' }}>
+            <Grid container spacing={0}>
+              <Box
+                px={{ xs: 1, sm: 3 }}
+                py={{ xs: 0, sm: 0 }}
+                sx={{
+                  display: 'flex',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  width: '100%',
+                  minHeight: '50vh',
+                  overflowY: 'hidden',
+                }}
+              >
+                <SyncLoader color="#1A73E8" />
+              </Box>
+            </Grid>
+          </Box>
+        </Grid>
+      )}
+      <Box
+        component="section"
+        py={{ xs: 2, sm: 6 }}
+        sx={mapLoaded ? { maxWidth: '100%', visibility: 'visible' } : { maxWidth: '100%', visibility:"hidden" }}
+      >
         <NoLocationFound toggle={errorMessage} />
 
         <Grid container item px={0}>
@@ -59,17 +87,11 @@ function FormWrapper({ form }) {
                       {documentReady && <MapExternal setMapLoaded={setMapLoaded} loaded={loaded} />}
                  
                     </div> */}
-                    <div className="map-container">
-                      <div id="static" style={mapLoaded ? { zIndex: 1 } : { zIndex: 3 }}>
-                        <LocationButtonIcon />
-                      </div>
-                      {documentReady && <MapExternal setMapLoaded={handleMapLoad} />}
-                    </div>
+                    <div className="map-container">{documentReady && <MapExternal setMapLoaded={handleMapLoad} />}</div>
                   </Box>
                 </Box>
               </Grid>
-          {/*     <Grid item xs={12} lg={5}><button type='button' onClick={() => setMapLoaded(!mapLoaded)}>
-              Toggle Map Visibility</button></Grid> */}
+            
             </Grid>
           </Box>
         </Grid>
