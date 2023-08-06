67b4c7d229a9dbe32606dd9c168ee7bc5964bf77
alogan5201 <drewthomaslogan5201@gmail.com>
2023-08-05 22:21:36 -0400

    up

--- src/pages/Movies/index.jsx
+++ src/pages/Movies/index.jsx
@@ -1,15 +1,16 @@
-import { getFunctions, httpsCallable } from 'firebase/functions';
 import { useEffect, useState } from 'react';
 import { Link, useNavigate, useParams } from 'react-router-dom';
 import { generateRanges, getMovieListLength, isInPaginationPosition } from 'util/helpers';
+import { httpsCallable, getFunctions } from 'firebase/functions';
 
 import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
 import Pagination from '@mui/material/Pagination';
 import Skeleton from '@mui/material/Skeleton';
 import Box from 'components/Box';
-import Typography from 'components/Typography';
 import BaseLayout from 'layouts/sections/components/BaseLayout';
+import Loading from 'components/Loading';
+import Typography from 'components/Typography';
 
 function getRangeForPage(n, pageIndex) {
   const ranges = generateRanges(n);
@@ -19,6 +20,7 @@ function MoviesPage() {
   const navigate = useNavigate();
   const { slug } = useParams();
   const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(true);
   const [imagesLoading, setImagesLoading] = useState(true);
   const [pagIndex, setPagIndex] = useState(null);
   const [paginationLength, setPaginationLength] = useState(null);
@@ -30,7 +32,7 @@ function MoviesPage() {
   const handlePagination = (e, page) => {
     navigate(`/movies/${page}`);
     setImagesLoading(true);
-   
+    setLoading(true);
   };
   useEffect(() => {
     if (imagesLoaded > 0 && movies.length > 0) {
@@ -47,21 +49,18 @@ function MoviesPage() {
   }, [imagesLoaded, movies]);
 
   useEffect(() => {
- 
+    setTimeout(() => {
+      setLoading(false);
+    }, 1000);
     setTimeout(() => {
       setImagesLoading(false);
     }, 2000);
-    
-  }, [ allImagesLoaded]);
+    return () => {
+      // eslint-disable-next-line no-unused-vars
+      setImagesLoaded((imagesLoaded) => (imagesLoaded = 0));
+    };
+  }, [loading, allImagesLoaded]);
 
-useEffect(() => {
-  if (imagesLoading) {
-    setTimeout(() => {
-      setImagesLoading(false);
-      
-    }, 2000);
-  }
-}, [imagesLoading]);
   async function getMoviesInRange(start, end) {
     try {
       const result = await fetchMoviesInRange({ start: start, end: end });
@@ -71,7 +70,6 @@ useEffect(() => {
       return movies;
     } catch (error) {
       // Getting the Error details.
-   
     }
 
     // const moviesCollection = collection(db, 'films');
@@ -114,83 +112,89 @@ useEffect(() => {
   }, [navigate, slug]);
 
   return (
-    <BaseLayout>
-        <Box component="section" py={6} sx={{ px: { xs: 0, lg: 20 } }}>
-          <Container>
-            <Grid container item xs={12} lg={6} flexDirection="column">
-              <Typography variant="h3" mt={3} mb={1}>
-                Movies Locations
-              </Typography>
-              <Typography variant="body2" color="text" mb={2}>
-                Discover where you&apos;re favorite flicks were filmed and bookmark them for your next trip!
-              </Typography>
-            </Grid>
-            {imagesLoading && (
-              <Grid container spacing={5} mt={3}>
-                {Array.from({ length: 15 }).map((_, index) => (
-                  <Grid key={index} item xs={12} lg={4}>
-                    <div style={{ paddingTop: '150%', position: 'relative' }}>
-                      <Skeleton
-                        animation="wave"
-                        variant="rectangular"
-                        sx={{
-                          position: 'absolute',
-                          top: 0,
-                          left: 0,
-                          width: '100%',
-                          height: '100%',
-                          borderRadius: '8px',
-                        }}
-                      />
-                    </div>
+    <>
+      {loading ? (
+        <Loading />
+      ) : (
+        <BaseLayout>
+          <Box component="section" py={6} sx={{ px: { xs: 0, lg: 20 } }}>
+            <Container>
+              <Grid container item xs={12} lg={6} flexDirection="column">
+                <Typography variant="h3" mt={3} mb={1}>
+                  Movies Locations
+                </Typography>
+                <Typography variant="body2" color="text" mb={2}>
+                  Discover where you&apos;re favorite flicks were filmed and bookmark them for your next trip!
+                </Typography>
+              </Grid>
+              {imagesLoading && (
+                <Grid container spacing={5} mt={3}>
+                  {Array.from({ length: 15 }).map((_, index) => (
+                    <Grid key={index} item xs={12} lg={4}>
+                      <div style={{ paddingTop: '150%', position: 'relative' }}>
+                        <Skeleton
+                          animation="wave"
+                          variant="rectangular"
+                          sx={{
+                            position: 'absolute',
+                            top: 0,
+                            left: 0,
+                            width: '100%',
+                            height: '100%',
+                            borderRadius: '8px',
+                          }}
+                        />
+                      </div>
+                    </Grid>
+                  ))}
+                </Grid>
+              )}
+
+              <Grid
+                container
+                spacing={5}
+                mt={3}
+                sx={imagesLoading ? { visibility: 'hidden' } : { visibility: 'visible' }}
+              >
+                {movies.map((data) => (
+                  <Grid key={data.id} item xs={12} lg={4}>
+                    <Link to={`/location/${data.slug}`}>
+                      <div style={{ paddingTop: '150%', position: 'relative' }}>
+                        <img
+                          src={data.image}
+                          alt={`${data.title} movie poster`}
+                          style={{
+                            position: 'absolute',
+                            top: 0,
+                            left: 0,
+                            width: '100%',
+                            height: '100%',
+                          }}
+                          onLoad={() => setImagesLoaded((imagesLoaded) => imagesLoaded + 1)}
+                        />
+                      </div>
+                      {/* Caption Underneath Image */}
+                      <Box sx={{ textAlign: 'center' }}>
+                        <Typography variant="caption" align="center">
+                          {data.title}
+                        </Typography>
+                      </Box>
+                    </Link>
                   </Grid>
                 ))}
               </Grid>
-            )}
-
-            <Grid
-              container
-              spacing={5}
-              mt={3}
-              sx={imagesLoading ? { visibility: 'hidden' } : { visibility: 'visible' }}
-            >
-              {movies.map((data) => (
-                <Grid key={data.id} item xs={12} lg={4}>
-                  <Link to={`/location/${data.slug}`}>
-                    <div style={{ paddingTop: '150%', position: 'relative' }}>
-                      <img
-                        src={data.image}
-                        alt={`${data.title} movie poster`}
-                        style={{
-                          position: 'absolute',
-                          top: 0,
-                          left: 0,
-                          width: '100%',
-                          height: '100%',
-                        }}
-                        onLoad={() => setImagesLoaded((imagesLoaded) => imagesLoaded + 1)}
-                      />
-                    </div>
-                    {/* Caption Underneath Image */}
-                    <Box sx={{ textAlign: 'center' }}>
-                      <Typography variant="caption" align="center">
-                        {data.title}
-                      </Typography>
-                    </Box>
-                  </Link>
+              <Box pt={6} px={1} mt={6}>
+                <Grid container justifyContent="center">
+                  {paginationLength && pagIndex && (
+                    <Pagination page={pagIndex} count={paginationLength} shape="rounded" onChange={handlePagination} />
+                  )}
                 </Grid>
-              ))}
-            </Grid>
-            <Box pt={6} px={1} mt={6}>
-              <Grid container justifyContent="center">
-                {paginationLength && pagIndex && (
-                  <Pagination page={pagIndex} count={paginationLength} shape="rounded" onChange={handlePagination} />
-                )}
-              </Grid>
-            </Box>
-          </Container>
-        </Box>
-      </BaseLayout>
+              </Box>
+            </Container>
+          </Box>
+        </BaseLayout>
+      )}
+    </>
   );
 }
 export default MoviesPage;
