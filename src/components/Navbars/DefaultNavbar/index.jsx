b618a71223c3355ce57f78297b53b17b2bdf74b5
alogan5201 <drewthomaslogan5201@gmail.com>
2023-11-05 03:05:41 -0500

    up

--- src/components/Navbars/DefaultNavbar/index.jsx
+++ src/components/Navbars/DefaultNavbar/index.jsx
@@ -454,7 +454,7 @@ function DefaultNavbar({ brand, routes, transparent, light, sticky, relative, ce
 }
 // Setting default values for the props of DefaultNavbar
 DefaultNavbar.defaultProps = {
-  brand: 'MovieLatLong',
+  brand: 'geotools',
   transparent: false,
   light: false,
   action: false,
